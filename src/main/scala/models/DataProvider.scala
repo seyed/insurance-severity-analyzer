a76d3155f45560de141e4e5a394c5b310d4186ec
seyed <seyed@presentter.com>
2018-10-14 17:42:34 +0800

    add highestClaim method that uses Spark sql and gets the highest claim from insurance, which is the temporary view of the dataframe

--- src/main/scala/models/DataProvider.scala
+++ src/main/scala/models/DataProvider.scala
@@ -28,6 +28,7 @@ class DataProvider {
 
     averageDamageClaim(spark)
     lowestClaim(spark)
+    highestClaim(spark)
 
   }
 
@@ -35,13 +36,14 @@ class DataProvider {
   def numberOfRows(dataFrame: DataFrame): Long = dataFrame.count
   def dataframeSnapShot(dataFrame: DataFrame): Unit = dataFrame.show()
 
-  def averageDamageClaim(spark: SparkSession): Unit = {
+  def averageDamageClaim(spark: SparkSession): Unit =
     spark.sql("SELECT avg(insurance.label) as Average_Lost From insurance").show()
-  }
 
-  def lowestClaim(spark: SparkSession): Unit = {
+
+  def lowestClaim(spark: SparkSession): Unit =
     spark.sql("SELECT min(insurance.label) as Minimum_Lost FROM insurance").show()
-  }
 
+  def highestClaim(spark: SparkSession): Unit =
+    spark.sql("SELECT max(insurance.label) as Maximum_Lost FROM insurance").show()
 
 }

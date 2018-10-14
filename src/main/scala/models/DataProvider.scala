d35a93bd2f436f1cf1be48314958aeb32b64be5d
seyed <seyed@presentter.com>
2018-10-14 17:29:22 +0800

    use spark sql to get the average lost within the given dataframe

--- src/main/scala/models/DataProvider.scala
+++ src/main/scala/models/DataProvider.scala
@@ -1,5 +1,6 @@
 package models
 
+import org.apache.spark
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 case class InsuranceClaimRecord(name: String, filledDate: String, value: Float)
@@ -17,14 +18,23 @@ class DataProvider {
       .withColumnRenamed("loss", "label")
       .cache
 
+
     println("Train data frame Schema ${dataframeSchema(trainDataframe)}")
     println(s"Number of rows: ${numberOfRows(trainDataframe)}")
     println(s"Dataframe snapshot:  \n ${dataframeSnapShot(trainDataframe)}")
+
+    averageDamageClaim(trainDataframe, spark)
 
   }
 
   def dataframeSchema(dataFrame: DataFrame): Unit = dataFrame.printSchema()
   def numberOfRows(dataFrame: DataFrame): Long = dataFrame.count
   def dataframeSnapShot(dataFrame: DataFrame): Unit = dataFrame.show()
+
+  def averageDamageClaim(df: DataFrame, spark: SparkSession): Unit = {
+    df.createOrReplaceTempView("insurance")
+    spark.sql("SELECT avg(insurance.label) as Average_Lost From insurance").show()
+  }
+
 
 }

d925950c83256266e4d1bd87cbb79866ec36373a
seyed <seyed@presentter.com>
2018-10-15 16:22:24 +0800

    update spark ml version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,5 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val sparkSql = "org.apache.spark" %% "spark-sql" % "2.3.1"
-  lazy val sparkMl = "org.apache.spark" %% "spark-mllib" % "2.3.1" % "runtime"
-
+  lazy val sparkMl =  "org.apache.spark" %% "spark-mllib" % "2.3.2" % "runtime"
 }
